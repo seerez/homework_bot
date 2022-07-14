526ce170ba642c02b0b4347d0de7850ed83173ae
Сергей Мдинарадзе <s@mdinaradze.ru>
2022-07-14 21:11:42 +0300

    проект готов

--- homework.py
+++ homework.py
@@ -1,11 +1,17 @@
-...
+import logging
+import os
+import time
+
+import requests
+import telegram
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -18,65 +24,92 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='.homework.log',
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
 
-def send_message(bot, message):
-    ...
+
+class HomeworksIsNotList(Exception):
+    """Класс исключения"""
+
+    pass
 
 
-def get_api_answer(current_timestamp):
+def send_message(bot, message) -> None:
+    """Отправка сообщения пользователю"""
+    bot.send_message(
+        chat_id=TELEGRAM_CHAT_ID,
+        text=message
+    )
+
+
+def get_api_answer(current_timestamp) -> dict:
+    """Получение ответа с сервера за определенный период времени"""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
+    try:
+        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    except Exception as error:
+        logging.error(f'Ошибка при запросе к API: {error}')
+    if response.status_code != 200:
+        raise Exception('API не ответил')
+    return response.json()
 
-    ...
 
-
-def check_response(response):
-
-    ...
+def check_response(response) -> list:
+    """Проверка ответа API на корректность"""
+    try:
+        homeworks = response['homeworks']
+    except KeyError as error:
+        logging.error(f'Ошибка доступа к ключу: {error}')
+    hw_list = response.get('homeworks', [])
+    if not isinstance(hw_list, list):
+        raise HomeworksIsNotList('The list of homeworks is not a list')
+    return homeworks
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
-
-    ...
-
-    verdict = ...
-
-    ...
-
+    """Получение статуса домашней работы"""
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    verdict = HOMEWORK_STATUSES[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
-def check_tokens():
-    ...
+def check_tokens() -> bool:
+    """Проверка переменных окружения"""
+    return all((PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID))
 
 
-def main():
+def main() -> None:
     """Основная логика работы бота."""
-
-    ...
-
+    if not check_tokens():
+        logging.critical('Отсутсвуют одна или несколько переменных окружения')
+        print("Программа остановлена")
+        os._exit(1)
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
-
-    ...
-
+    current_timestamp = int(1656152278)
     while True:
         try:
-            response = ...
-
-            ...
-
-            current_timestamp = ...
+            response = get_api_answer(current_timestamp)
+            homework = check_response(response)
+            list_homeworks = check_response(response)
+            logging.info(f'Найдено{len(list_homeworks)} работ')
+            if len(homework) == 0:
+                print('Список домашних заданий пуст')
+            else:
+                status = parse_status(homework[0])
+                send_message(bot, status)
+            current_timestamp = response['current_date']
             time.sleep(RETRY_TIME)
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
-            time.sleep(RETRY_TIME)
-        else:
-            ...
+            logging.error(message)
+            send_message(bot, message)
+        time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
